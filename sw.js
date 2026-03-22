3fe44a057015d0fb8a4390733c5ee79da5cf5f8f
satchayasong-source <satchayasong@gmail.com>
2026-03-22 16:28:41 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-const CACHE_NAME = 'fixel-pro-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;600&display=swap'
-];
+const CACHE_NAME = 'fixel-pro-v10';
+const assets = ['./', './index.html'];
 
-// ติดตั้งและเก็บไฟล์ลง Cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// ดึงข้อมูลจาก Cache เมื่อเปิดแอป
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
